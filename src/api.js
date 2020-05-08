46fe2650f5933d0a4f739b89d47ca61eea7a0cf0
abc8806191 <49439251+abc8806191@users.noreply.github.com>
2020-05-08 16:28:18 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -40,8 +40,8 @@ class LineAPI {
     path: this.config.LINE_HTTP_URL,
     https: true
   }) {
-    //options.headers['X-Line-Application'] = 'CHROMEOS\t2.1.0\tChrome_OS\t1';
-    options.headers['X-Line-Application'] = 'IOSIPAD 7.14.0 iPhone OS 10.12.0';
+    options.headers['X-Line-Application'] = 'CHROMEOS\t2.1.0\tChrome_OS\t1';
+    //options.headers['X-Line-Application'] = 'IOSIPAD 7.14.0 iPhone OS 10.12.0';
     //options.headers['X-Line-Application'] = 'DESKTOPMAC\t5.3.3-YOSEMITE-x64\tMAC\t10.12.0';
     this.options = options;
     this.connection =
